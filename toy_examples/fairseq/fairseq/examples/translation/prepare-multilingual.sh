cb6c498fe88341d561e81313b84d2b24e488f757
Andrei Gidea <andrei-cristian.gidea@my.fmi.unibuc.ro>
2022-04-28 11:41:56 +0300

    adaugare sh uri

---
+++ toy_examples/fairseq/fairseq/examples/translation/prepare-multilingual.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+echo 'Cloning Moses github repository (for tokenization scripts)...'
+git clone https://github.com/moses-smt/mosesdecoder.git
+
+echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
+git clone https://github.com/rsennrich/subword-nmt.git
+
+SCRIPTS=mosesdecoder/scripts
+TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
+LC=$SCRIPTS/tokenizer/lowercase.perl
+CLEAN=$SCRIPTS/training/clean-corpus-n.perl
+NORM_PUNC=$SCRIPTS/tokenizer/normalize-punctuation.perl
+REM_NON_PRINT_CHAR=$SCRIPTS/tokenizer/remove-non-printing-char.perl
+BPEROOT=subword-nmt/subword_nmt
+SPM_TRAIN=$BPEROOT/learn_bpe.py
+SPM_ENCODE=$BPEROOT/apply_bpe.py
+BPE_TOKENS=16000
+
+URLS=(
+    "http://statmt.org/wmt13/training-parallel-europarl-v7.tgz"
+    "http://statmt.org/wmt14/test-full.tgz"
+)
+ARCHIVES=(
+    "training-parallel-europarl-v7.tgz"
+    "test-full.tgz"
+)
+CORPORA=(
+    "training/europarl-v7.de-en"
+    "training/europarl-v7.fr-en"
+)
+
+if [ ! -d "$SCRIPTS" ]; then
+    echo "Please set SCRIPTS variable correctly to point to Moses scripts."
+    exit
+fi
+
+SRCS=(
+    "de"
+    "fr"
+)
+TGT=en
+ORIG=multilingual_orig
+DATA=multilingual.de_fr.en.bpe16k
+TMP=$DATA/tmp
+
+mkdir -p $ORIG $TMP $DATA
+
+cd $ORIG
+
+# # download and extract data
+# for ((i=0;i<${#URLS[@]};++i)); do
+#     ARCHIVE=${ARCHIVES[i]}
+#     if [ -f "$ARCHIVE" ]; then
+#         echo "$ARCHIVE already exists, skipping download"
+#     else
+#         URL=${URLS[i]}
+#         wget "$URL"
+#         if [ -f "$ARCHIVE" ]; then
+#             echo "$URL successfully downloaded."
+#         else
+#             echo "$URL not successfully downloaded."
+#             exit 1
+#         fi
+#     fi
+#     FILE=${ARCHIVE: -4}
+#     if [ -e $FILE ]; then
+#         echo "$FILE already exists, skipping extraction"
+#     else
+#         if [ $FILE == ".tgz" ]; then
+#             tar zxvf $ARCHIVE
+#         elif [ $FILE == ".tar" ]; then
+#             tar xvf $ARCHIVE
+#         fi
+#         # tar -xzvf $ARCHIVE
+#     fi
+# done
+
+cd ..
+
+# echo "pre-processing train data..."
+# for SRC in "${SRCS[@]}"; do
+#     for LANG in $SRC $TGT; do
+#         EXT=${SRC}-${TGT}
+#         TOK=train.tags.$EXT.tok.$LANG
+#         rm $TMP/$TOK
+#         for f in "${CORPORA[@]}"; do
+#             cat $ORIG/$f.$LANG | \
+#                 perl $NORM_PUNC $LANG | \
+#                 perl $REM_NON_PRINT_CHAR | \
+#                 perl $TOKENIZER -threads 8 -a -l $LANG >> $TMP/$TOK
+#         done
+#     done
+# done
+
+# # Clean inainte sau dupa BPE???
+# ### for SRC in "${SRCS[@]}"; do
+# #     EXT=${SRC}-${TGT}
+# #     perl $CLEAN -ratio 1.5 $TMP/train.tags.$EXT.tok $SRC $TGT $TMP/train.tags.$EXT.tok 1 250
+# #     perl $CLEAN -ratio 1.5 $TMP/valid.tags.$EXT.tok $SRC $TGT $TMP/valid.tags.$EXT.tok 1 250
+# #### done
+
+# echo "pre-processing test data..."
+# for SRC in "${SRCS[@]}"; do
+#     for LANG in $SRC $TGT; do
+#         if [ "$LANG" == "$SRC" ]; then
+#             t="src"
+#         else
+#             t="ref"
+#         fi
+#         grep '<seg id' $ORIG/test-full/newstest2014-${SRC}${TGT}-$t.$LANG.sgm | \
+#             sed -e 's/<seg id="[0-9]*">\s*//g' | \
+#             sed -e 's/\s*<\/seg>\s*//g' | \
+#             sed -e "s/\’/\'/g" | \
+#         perl $TOKENIZER -threads 8 -a -LANG $LANG > $TMP/test.${SRC}-${TGT}.$LANG
+#         echo ""
+#     done
+# done
+
+# echo "splitting train and valid..."
+# for SRC in "${SRCS[@]}"; do
+#     for LANG in $SRC $TGT; do
+#         TOK=train.tags.${SRC}-${TGT}.tok.$LANG
+#         EXT=${SRC}-${TGT}.$LANG
+#         awk '{if (NR%1333 == 0)  print $0; }' $TMP/$TOK > $TMP/valid.$EXT
+#         awk '{if (NR%1333 != 0)  print $0; }' $TMP/$TOK > $TMP/train.$EXT
+#     done
+# done
+
+
+# # for SRC in "${SRCS[@]}"; do
+# #     TRAIN=$TMP/train.$SRC-$TGT
+# #     BPE_CODE=$DATA/code.$SRC-$TGT
+# #     rm -f $TRAIN
+# #     for LANG in $SRC $TGT; do
+# #         cat $TMP/train.${SRC}-${TGT}.$LANG >> $TRAIN
+# #     done
+# # done
+
+ALL_SRCS=${SRCS[*]}
+JOINED_SRCS=${ALL_SRCS// /_}
+TRAIN=$TMP/train.$JOINED_SRCS-$TGT
+
+rm -f $TRAIN
+for SRC in "${SRCS[@]}"; do
+    for LANG in $SRC $TGT; do
+        cat $TMP/train.${SRC}-${TGT}.$LANG >> $TRAIN
+    done
+done
+
+BPE_CODE=$DATA/code.$JOINED_SRCS-$TGT
+echo "learn_bpe.py on ${TRAIN}..."
+python $SPM_TRAIN -s $BPE_TOKENS < $TRAIN > $BPE_CODE
+
+for SRC in "${SRCS[@]}"; do
+    for LANG in $SRC $TGT; do
+        EXT=${SRC}-${TGT}.$LANG
+        for f in train.$EXT valid.$EXT test.$EXT; do
+            echo "apply_bpe.py to ${f}..."
+            python $SPM_ENCODE -c $BPE_CODE < $TMP/$f > $TMP/bpe.$f
+        done
+    done
+done
+
+for SRC in "${SRCS[@]}"; do
+    EXT=${SRC}-${TGT}
+    perl $CLEAN -ratio 1.5 $TMP/bpe.train.$EXT $SRC $TGT $DATA/train.$EXT 1 250
+    perl $CLEAN -ratio 1.5 $TMP/bpe.valid.$EXT $SRC $TGT $DATA/valid.$EXT 1 250
+done
+
+for SRC in "${SRCS[@]}"; do
+    for LANG in $SRC $TGT; do
+        EXT=${SRC}-${TGT}.$LANG
+        cp $TMP/bpe.test.$EXT $DATA/test.$EXT
+    done
+done
---
+++ toy_examples/fairseq/fairseq/multi_translation.sh
@@ -0,0 +1,86 @@
+# First install sacrebleu and sentencepiece
+pip install sacrebleu sentencepiece
+
+# Then download and preprocess the data
+cd examples/translation/
+# bash prepare-multilingual.sh
+cd ../..
+
+SRCS=(
+    "de"
+    "fr"
+)
+SRC=de
+TGT=en
+
+ALL_SRCS=${SRCS[*]}
+JOINED_SRCS=${ALL_SRCS// /_}
+LANG_PAIRS="${SRCS[@]/%/-$TGT}"
+LANG_PAIRS=${LANG_PAIRS// /,}
+echo $LANG_PAIRS
+
+# Binarize the de-en dataset
+TEXT=examples/translation/multilingual.$JOINED_SRCS.$TGT.bpe16k
+# fairseq-preprocess --source-lang $SRC --target-lang $TGT \
+#     --trainpref $TEXT/train.$SRC-$TGT \
+#     --validpref $TEXT/valid.$SRC-$TGT \
+#     --destdir data-bin/multilingual.$JOINED_SRCS.$TGT.bpe16k \
+#     --workers 10
+
+SRC=fr
+# # Binarize the fr-en dataset
+# # NOTE: it's important to reuse the en dictionary from the previous step
+# fairseq-preprocess --source-lang $SRC --target-lang $TGT \
+#     --trainpref $TEXT/train.$SRC-$TGT \
+#     --validpref $TEXT/valid.$SRC-$TGT \
+#     --tgtdict data-bin/multilingual.$JOINED_SRCS.$TGT.bpe16k/dict.$TGT.txt \
+#     --destdir data-bin/multilingual.$JOINED_SRCS.$TGT.bpe16k \
+#     --workers 10
+
+# Train a multilingual transformer model
+# NOTE: the command below assumes 1 GPU, but accumulates gradients from
+#       8 fwd/bwd passes to simulate training on 8 GPUs
+mkdir -p checkpoints/multilingual_transformer_$JOINED_SRCS
+CUDA_VISIBLE_DEVICES=0 fairseq-train data-bin/multilingual.$JOINED_SRCS.$TGT.bpe16k/ \
+    --max-epoch 1 \
+    --task multilingual_translation --lang-pairs $LANG_PAIRS \
+    --arch multilingual_transformer_iwslt_de_en \
+    --share-decoders --share-decoder-input-output-embed \
+    --optimizer adam --adam-betas '(0.9, 0.98)' \
+    --lr 0.0005 --lr-scheduler inverse_sqrt \
+    --warmup-updates 4000 --warmup-init-lr '1e-07' \
+    --label-smoothing 0.1 --criterion label_smoothed_cross_entropy \
+    --dropout 0.3 --weight-decay 0.0001 \
+    --save-dir checkpoints/multilingual_transformer_$JOINED_SRCS \
+    --tensorboard-logdir checkpoints/multilingual_transformer_$JOINED_SRCS/log-tb \
+    --max-tokens 4000 \
+    --update-freq 8 \
+    --fp16 \
+    --eval-bleu \
+    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-detok moses \
+    --eval-bleu-remove-bpe=sentencepiece \
+    --eval-bleu-print-samples \
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric
+    # --ddp-backend=legacy_ddp \
+
+# # Generate and score the test set with sacrebleu
+# SRC=de
+# # sacrebleu --test-set iwslt17 --language-pair ${SRC}-$TGT --echo src \
+# #     | python scripts/spm_encode.py --model examples/translation/multilingual.$JOINED_SRCS.$TGT.bpe16k/sentencepiece.bpe.model \
+# #     > iwslt17.test.${SRC}-$TGT.${SRC}.bpe
+# cat $TEXT/test.$SRC-$TGT.$SRC \
+#     | fairseq-interactive data-bin/multilingual.$JOINED_SRCS.$TGT.bpe16k/ \
+#       --task multilingual_translation --lang-pairs $LANG_PAIRS \
+#       --source-lang ${SRC} --target-lang $TGT \
+#       --path checkpoints/multilingual_transformer_$JOINED_SRCS/checkpoint_best.pt \
+#       --buffer-size 2000 --batch-size 128 \
+#       --beam 5 --remove-bpe=sentencepiece \
+#     > $TEXT/test.$SRC-$TGT.$TGT.sys
+# grep ^H $TEXT/test.$SRC-$TGT.$TGT.sys | cut -f3 \
+#     | sacrebleu $TEXT/test.$SRC-$TGT.$TGT
+# # grep ^H $TEXT/test.$SRC-$TGT.$TGT.sys | cut -f3 > $TEXT/test.$SRC-$TGT.$TGT.hyp
+
+# # fairseq-generate data-bin/multilingual.$JOINED_SRCS.$TGT.bpe16k \
+# #     --path checkpoints/multilingual_transformer_$JOINED_SRCS/checkpoint_best.pt \
+# #     --batch-size 128 --beam 5 --remove-bpe
